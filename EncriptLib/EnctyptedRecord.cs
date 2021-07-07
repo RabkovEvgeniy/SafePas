35a9920a63c0df46abde1cbc2fc36904b99d99bf
Rabkov Evgeniy <rabkov_evgeniy@mail.ru>
2021-07-08 00:56:46 +0700

    Add EncryptedRecord

---
+++ EncriptLib/EnctyptedRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncriptLib
+{
+    public class EncryptedRecord
+    {
+        public string Value { get; private set; }
+        public bool isEncrypted{ get; private set; }
+        // public Id KeyID { get; private set; }
+        // public void Encrypt(Key key) { };
+
+    }
+}
