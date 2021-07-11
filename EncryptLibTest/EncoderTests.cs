01cfe03c5c5d0cf2b1bf975d5578aacf4e506be2
Rabkov Evgeniy <rabkov_evgeniy@mail.ru>
2021-07-11 18:22:48 +0700

    Add test for Encoder.Decrypt method

--- EncryptLibTest/EncoderTests.cs
+++ EncryptLibTest/EncoderTests.cs
@@ -43,5 +43,44 @@ namespace EncryptLibTests
             Assert.AreEqual(encryptedPassword, smallKeyResult);
             Assert.AreEqual(encryptedPasswordWithWholePartDiscarded, bigKeyResult);
         }
+
+        [TestMethod]
+        public void DecryptTest()
+        {
+            //Arrange
+            string encryptedPasswordWithWholePartDiscarded, encryptedPassword;
+            const string password = "11111";
+            string decryptWithSmallKeyResult, decryptWithBigKeyResult;
+
+            int[] keyValue = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                keyValue[i] = 1;
+            }
+            EncryptionKey smallKey = new EncryptionKey()
+            {
+                Value = keyValue
+            };
+
+            for (int i = 0; i < 100; i++)
+            {
+                keyValue[i] = 1103;
+            }
+            EncryptionKey bigKey = new EncryptionKey()
+            {
+                Value = keyValue
+            };
+
+            encryptedPassword = Encoder.Encrypt(password, smallKey);
+            encryptedPasswordWithWholePartDiscarded = Encoder.Encrypt(password, bigKey);
+
+            //Act
+            decryptWithSmallKeyResult = Encoder.Decrypt(encryptedPassword, smallKey);
+            decryptWithBigKeyResult = Encoder.Decrypt(encryptedPasswordWithWholePartDiscarded, bigKey);
+
+            //Assert
+            Assert.AreEqual(decryptWithSmallKeyResult,password);
+            Assert.AreEqual(decryptWithBigKeyResult,password);
+        }
     }
 }
